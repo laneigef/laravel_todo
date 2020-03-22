1d5846067e962b0ae50c9f892f36641dfcf405e8
La_neige <laneigef@gmail.com>
2020-03-22 19:21:51 +0900

    Heroku Commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        // Herokuでhttpsを有効
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
